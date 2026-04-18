efb1a62e86ecb142572c7bae2e60b6a42d320fab
Mike Nelson <mjn.other@gmail.com>
2026-04-18 09:43:29 -0500

    Qtfred fixes and tweaks (#7379)
    * fix mission stats message count
    
    * fix texture replace for retail textures
    
    * fix heap issue in ship alt class
    
    * fix props menu item
    
    * some ui clarity
    
    * unify up/down buttons to use our painted icons
    
    * fixup ship and wing cues toggles
    
    * rename and move Object Orientation menu item
    
    * menu seperator styling for dark mode

--- qtfred/src/ui/FredView.cpp
+++ qtfred/src/ui/FredView.cpp
@@ -124,10 +124,6 @@ FredView::FredView(QWidget* parent) : QMainWindow(parent), ui(new Ui::FredView()
 
 	initializeGroupActions();
 
-	auto propsAction = new QAction(tr("Props"), this);
-	connect(propsAction, &QAction::triggered, this, &FredView::on_actionProps_triggered);
-	ui->menuObjects->insertAction(ui->actionWaypoint_Paths, propsAction);
-
 	connect(ui->actionPreferences, &QAction::triggered, this, [this]() {
 		dialogs::PreferencesDialog preferencesDialog(this, _viewport);
 		preferencesDialog.exec();
@@ -1638,7 +1634,7 @@ void FredView::on_actionCampaign_triggered(bool) {
 	editorCampaign->setAttribute(Qt::WA_DeleteOnClose);
 	editorCampaign->show();
 }
-void FredView::on_actionObjects_triggered(bool) {
+void FredView::on_actionObject_Orientation_triggered(bool) {
 	orientEditorTriggered();
 }
 void FredView::on_actionCommand_Briefing_triggered(bool) {
@@ -1783,7 +1779,7 @@ void FredView::orientEditorTriggered() {
 	dialog->exec();
 }
 void FredView::onUpdateEditorActions() {
-	ui->actionObjects->setEnabled(query_valid_object(fred->currentObject));
+	ui->actionObject_Orientation->setEnabled(query_valid_object(fred->currentObject));
 
 	const bool validObject = query_valid_object(fred->currentObject);
 	const bool hasMarked = fred->getNumMarked() > 0;
@@ -1796,7 +1792,7 @@ void FredView::onUpdateEditorActions() {
 	ui->actionDelete_Wing->setEnabled(fred->cur_wing >= 0);
 
 	// Objects editor — requires a selected object
-	ui->actionObjects->setEnabled(validObject);
+	ui->actionObject_Orientation->setEnabled(validObject);
 
 	// Level/Align — require something to be selected
 	ui->actionLevel_Object->setEnabled(validObject);
